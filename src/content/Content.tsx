22b853115bab11e11d400a844c0de812497fda88
MinhNTQ <35948603+ngthquocminh@users.noreply.github.com>
2024-03-21 16:56:47 +0700

    Update Content.tsx

--- src/content/Content.tsx
+++ src/content/Content.tsx
@@ -7,30 +7,6 @@ type IContentProps = {
 const Content = (props: IContentProps) => (
   <div className="content sun-editor-editable">
     {props.children}
-
-    <style jsx>
-      {`
-        /*.content :global(*) {
-          @apply break-words;
-        }
-
-        .content :global(p) {
-          @apply my-6;
-        }
-
-        .content :global(ul) {
-          @apply my-6;
-        }
-
-        .content :global(h2) {
-          @apply text-2xl font-semibold text-gray-700 my-4;
-        }
-
-        .content :global(h3) {
-          @apply text-xl font-semibold text-gray-700 my-4;
-        }*/
-      `}
-    </style>
   </div>
 );
 
